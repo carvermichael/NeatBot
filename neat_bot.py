861d3d0f778e5fe06f84e26ea5ddb8d59412b667
Ethan Schroeder <elschroeder13@gmail.com>
2017-06-26 23:08:21 -0500

    starting to add giphy api integration

--- neat_bot.py
+++ neat_bot.py
@@ -1,9 +1,17 @@
 import discord
 import asyncio
 import properties
+import urllib.request
+import json
 
 client = discord.Client()
+giphyApiBase = "http://api.giphy.com/v1/gifs/"
+giphyApiKey = properties.giphyToken
 
+def getJson(url):
+    response = urllib.request.urlopen(url)
+    data = json.load(response)
+    return data    
 
 @client.event
 async def on_ready():
@@ -18,6 +26,11 @@ async def on_message(message):
     if message.content.lower() == '!neat':
         response = '<@' + message.author.id + '> NEAT!'
         await client.send_message(message.channel, response)
+    elif message.content.lower() == 'hey neatbot':
+        gifData =  getJson(giphyApiBase + "random?api_key=" + giphyApiKey + "&tag=hello&rating=G")             
+        response = gifData["data"]["url"]
+        await client.send_message(message.channel, response)
+
 
 
 client.run(properties.token)
