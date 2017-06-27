a8c665333ecbbfb67593fbcbab30e6f5ca9da44c
Ethan Schroeder <elschroeder13@gmail.com>
2017-06-27 00:27:12 -0500

    adding search and trending capabilities

--- neat_bot.py
+++ neat_bot.py
@@ -7,11 +7,23 @@ import json
 client = discord.Client()
 giphyApiBase = "http://api.giphy.com/v1/gifs/"
 giphyApiKey = properties.giphyToken
+gifTriggerString= "gif me"
+topTrendingGifTrigger = "top trending gif"
 
 def getJson(url):
     response = urllib.request.urlopen(url)
     data = json.load(response)
     return data    
+
+def getGif(searchTerm):
+    gifData = getJson(giphyApiBase + "random?api_key=" + giphyApiKey + "&tag="+searchTerm+"&rating=R")
+    gifUrl = gifData["data"]["url"]
+    return gifUrl
+
+def getTrendingGif(gifNumber, maxRecords):
+    gifData = getJson(giphyApiBase + "trending?api_key="+giphyApiKey+"&limit="+maxRecords+"&rating=R")
+    gifUrl = gifData["data"][gifNumber]["url"]
+    return gifUrl
 
 @client.event
 async def on_ready():
@@ -23,14 +35,21 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    response = ""
     if message.content.lower() == '!neat':
         response = '<@' + message.author.id + '> NEAT!'
-        await client.send_message(message.channel, response)
     elif message.content.lower() == 'hey neatbot':
-        gifData =  getJson(giphyApiBase + "random?api_key=" + giphyApiKey + "&tag=hello&rating=G")             
-        response = gifData["data"]["url"]
-        await client.send_message(message.channel, response)
+        response = gitGif("hello")
+    elif message.content.startswith(gifTriggerString):
+        searchTerm = ""
+        if len(message.content) > len(gifTriggerString):
+            searchTerm = message.content[len(gifTriggerString):].lstrip()
+        response = getGif(searchTerm)
+    elif message.content.startswith(topTrendingGifTrigger):
+        response = getTrendingGif(0, "1")
 
+    if response != "":
+        await client.send_message(message.channel, response)
 
 
 client.run(properties.token)
